6a03ad3446449c08ed06c6dff8687cc37c805a05
Ku6epXBOCTuK <Ku6epXBOCTuK@gmail.com>
2026-09-13 21:46:30 +0500

    style: prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import adapter from "@sveltejs/adapter-static";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
@@ -8,11 +8,11 @@ export default defineConfig({
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
-				experimental: { async: true }
+					filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+				experimental: { async: true },
 			},
 			adapter: adapter(),
-			experimental: { remoteFunctions: true }
-		})
-	]
+			experimental: { remoteFunctions: true },
+		}),
+	],
 });
